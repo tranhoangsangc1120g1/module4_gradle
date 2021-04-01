d5005aeb1cc7fdfe2eccee62a60dffa737451e64
unknown <thjsang6596@gmail.com>
2021-04-01 22:55:05 +0700

    fix ngay 4

--- Module04/ngay4/baitap/QuanLy_Sp/src/main/java/com/exercise/controller/ProductController.java
+++ Module04/ngay4/baitap/QuanLy_Sp/src/main/java/com/exercise/controller/ProductController.java
@@ -20,39 +20,39 @@ public class ProductController {
     public String getHome(Model model){
         List<Product> productList=productService.findAll();
         model.addAttribute("productList",productList);
-        return "/product/index";
+        return "index";
     }
-    @GetMapping("/product/create")
+    @GetMapping("/products/create")
     public String create(Model model) {
         model.addAttribute("product", new Product());
-        return "/product/create";
+        return "create";
     }
-    @PostMapping("/product/save")
+    @PostMapping("/products/save")
     public String save(Product product) {
         product.setId((int)(Math.random() * 10000));
         productService.save(product);
         return "redirect:/";
     }
-    @GetMapping("/product/{id}/edit")
+    @GetMapping("/products/{id}/edit")
     public String edit(@PathVariable int id, Model model) {
         model.addAttribute("product", productService.findById(id));
-        return "/product/edit";
+        return "edit";
     }
-    @PostMapping("/product/update")
+    @PostMapping("/products/update")
     public String update(Product product) {
         productService.update(product.getId(), product);
         return "redirect:/";
     }
-    @PostMapping("/product/delete")
+    @PostMapping("/products/delete")
     public String delete(Product product, RedirectAttributes redirect) {
         productService.remove(product.getId());
         redirect.addFlashAttribute("success", "Removed product successfully!");
         return "redirect:/";
     }
-    @GetMapping("/product/{id}/view")
+    @GetMapping("/products/{id}/view")
     public String view(@PathVariable int id, Model model) {
         model.addAttribute("product", productService.findById(id));
-        return "/product/view";
+        return "view";
     }
 
 }
