fe464e3c508d4965f58473459e55435d4f92ee2d
unknown <thjsang6596@gmail.com>
2021-04-01 08:26:54 +0700

    fix

--- Module04/ngay1/BAITAP/BAI1_CHUYENDOITIEN/src/main/java/com/controller/ExampleController.java
+++ Module04/ngay1/BAITAP/BAI1_CHUYENDOITIEN/src/main/java/com/controller/ExampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Example {
+public class ExampleController {
     @GetMapping("/")
     public String example() {
         return "/example";
