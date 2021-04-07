86845c99c4d8d8ff082ae3f4da08977c6140ccaa
unknown <thjsang6596@gmail.com>
2021-04-07 21:44:40 +0700

    fix

--- Module04/ngay8/baitap_8/src/main/java/com/example/baitap/controller/CustomerController.java
+++ Module04/ngay8/baitap_8/src/main/java/com/example/baitap/controller/CustomerController.java
@@ -2,27 +2,35 @@ package com.example.baitap.controller;
 
 import com.example.baitap.model.Customer;
 import com.example.baitap.service.ICusService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Controller
 public class CustomerController {
     @Autowired
     ICusService cusService;
-    @GetMapping("/create_cus")
+    @GetMapping("/create_cusform")
     public String showCreateForm(Model model){
         model.addAttribute("customer",new Customer());
         return "create";
     }
-
-    @PostMapping("/create_cus")
-    public String createArticle(Integer id, RedirectAttributes redirect){
-        cusService.deleteById(id);
-        redirect.addFlashAttribute("message", "Create successfully ");
-        return "redirect:/create_cus";
+    @PostMapping("/validate")
+    public ModelAndView checkValidation(@Valid Customer customer, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            ModelAndView modelAndView = new ModelAndView("create");
+            return modelAndView;
+        }
+        cusService.save(customer);
+        ModelAndView modelAndView = new ModelAndView("result", "customer", customer);
+        return modelAndView;
     }
-}
+    }
+
